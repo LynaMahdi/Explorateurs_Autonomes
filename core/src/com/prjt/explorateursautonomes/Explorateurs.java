69c370f0af4c3bf9787f6e450837d6b717c84a80
Lyna MAHDI <kl_mahdi@esi.dz>
2024-03-18 12:04:32 +0100

    collisions

--- core/src/com/prjt/explorateursautonomes/Explorateurs.java
+++ core/src/com/prjt/explorateursautonomes/Explorateurs.java
@@ -1,53 +1,83 @@
 package com.prjt.explorateursautonomes;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.graphics.Camera;
-import com.prjt.explorateursautonomes.world.GameMap;
-import com.prjt.explorateursautonomes.world.TileType;
-import com.prjt.explorateursautonomes.world.TiledGameMap;
+import com.prjt.explorateursautonomes.joueur.Joueur;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+
+
 public class Explorateurs extends ApplicationAdapter {
 
-
 	SpriteBatch batch;
-	Texture img;
-	GameMap gameMap;
+	Texture img; //image of the explorateur
 	TiledMap tiledMap;
 	OrthogonalTiledMapRenderer tmr;
-	OrthographicCamera camera=new OrthographicCamera();
+
+	TiledMapTileLayer collisionLayer;
+
+	OrthographicCamera camera = new OrthographicCamera();
+	Joueur joueur;
 
 	@Override
-	public void create () {
-		tiledMap=new TmxMapLoader().load("map.tmx");
-		tmr= new OrthogonalTiledMapRenderer(tiledMap);
-		camera.setToOrtho(false,512,512);
+	public void create() {
+		batch = new SpriteBatch();
+
+		// Initialize your TiledMap
+		tiledMap = new TmxMapLoader().load("map.tmx");
+		tmr = new OrthogonalTiledMapRenderer(tiledMap);
+		camera.setToOrtho(false, 1000, 950);
+
+		// Initialize the texture for the player
+		img = new Texture("crab_rest.png");
+
+
+		// Initialize the player instance
+		joueur = new Joueur(100, 10, 5, 6, 6, 1); // Example parameters
+
 	}
 
+
 	@Override
-	public void render () {
+	public void render() {
+		joueur.updatePreviousPosition();
+		joueur.updatePosition();
+
+		// les collisions avec les murs
+		checkCollisionWithObstacleLayer("mur supp2");
+		checkCollisionWithObstacleLayer("obstacle");
+		// Add more layers as needed
+
+		// Move the camera to follow the player
+		camera.position.set(joueur.getX(), joueur.getY(), 0);
+
+		// Render the Tiled map
 		tmr.setView(camera);
 		tmr.render();
+
+		// Render the player
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(img, joueur.getX(), joueur.getY());
+		batch.end();
 	}
-	
+
+	private void checkCollisionWithObstacleLayer(String layerName) {
+		TiledMapTileLayer obstacleLayer = (TiledMapTileLayer) tiledMap.getLayers().get(layerName);
+		if (joueur.isCollision(obstacleLayer, joueur.getX(), joueur.getY())) {
+			joueur.handleCollisionWithObstacle(obstacleLayer);
+		}
+	}
 	@Override
-	public void dispose () {
-
+	public void dispose() {
+		batch.dispose();
+		tiledMap.dispose();
+		tmr.dispose();
+		img.dispose();
 	}
-
-
-
-	public SpriteBatch getBatch() {
-		return batch;
-	}
-
 }
--- core/src/com/prjt/explorateursautonomes/joueur/Joueur.java
+++ core/src/com/prjt/explorateursautonomes/joueur/Joueur.java
@@ -1,57 +1,134 @@
 package com.prjt.explorateursautonomes.joueur;
 
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.MathUtils;
 import com.prjt.explorateursautonomes.monstre.Monstre;
+import com.prjt.explorateursautonomes.world.GameMap;
+
+
 public class Joueur {
 
-        // Attributs
-        private int pointsDeVie;
-        private int degats;
-        private double rangeDetection;
+    // Attributs
+    private int pointsDeVie;
+    private float prevX;
+    private float prevY;
 
-        // Constructeur
-        public Joueur(int pointsDeVie, int degats, double rangeDetection) {
-            this.pointsDeVie = pointsDeVie;
-            this.degats = degats;
-            this.rangeDetection = rangeDetection;
-        }
+    private int degats;
+    private double rangeDetection;
+    private float x;
+    private float y;
+    private float speed;
 
-        // Méthode pour attaquer un monstre
-        public void attaquer(Monstre monstre) {
-            monstre.recevoirDegats(this.degats);
-        }
+    // Constructeur
+    public Joueur(int pointsDeVie, int degats, double rangeDetection, float x, float y, float speed) {
+        this.pointsDeVie = pointsDeVie;
+        this.degats = degats;
+        this.rangeDetection = rangeDetection;
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
 
-        // Méthode pour recevoir des dégâts
-        public void recevoirDegats(int degats) {
-            this.pointsDeVie -= degats;
-            if (this.pointsDeVie <= 0) {
-                System.out.println("L'explorateur a été vaincu!");
-                // Modif l'addon si mort
-            }
-        }
+    // Méthode pour attaquer un monstre
+    public void attaquer(Monstre monstre) {
+        monstre.recevoirDegats(this.degats);
+    }
 
-        // Getters et setters
-        public int getPointsDeVie() {
-            return pointsDeVie;
+    // Méthode pour recevoir des dégâts
+    public void recevoirDegats(int degats) {
+        this.pointsDeVie -= degats;
+        if (this.pointsDeVie <= 0) {
+            System.out.println("L'explorateur a été vaincu!");
+            // Modif l'addon si mort
         }
+    }
 
-        public void setPointsDeVie(int pointsDeVie) {
-            this.pointsDeVie = pointsDeVie;
-        }
+    // Getters et setters
+    public int getPointsDeVie() {
+        return pointsDeVie;
+    }
 
-        public int getDegats() {
-            return degats;
-        }
+    public void setPointsDeVie(int pointsDeVie) {
+        this.pointsDeVie = pointsDeVie;
+    }
 
-        public void setDegats(int degats) {
-            this.degats = degats;
-        }
+    public int getDegats() {
+        return degats;
+    }
 
-        public double getRangeDetection() {
-            return rangeDetection;
-        }
+    public void setDegats(int degats) {
+        this.degats = degats;
+    }
 
-        public void setRangeDetection(double rangeDetection) {
-            this.rangeDetection = rangeDetection;
-        }
+    public double getRangeDetection() {
+        return rangeDetection;
+    }
+
+    public void setRangeDetection(double rangeDetection) {
+        this.rangeDetection = rangeDetection;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    // Méthode pour démarrer le mouvement autonome
+    public void updatePosition() {
+        // Simulate autonomous movement
+        x += speed;
+        y += speed;
+
+        float mapWidth = 950;
+        float mapHeight = 950;
+        float playerWidth = 50;
+        float playerHeight = 50;
+
+        // ne pas depasser la limite de la map
+        x = MathUtils.clamp(x, 0, mapWidth - playerWidth);
+        y = MathUtils.clamp(y, 0, mapHeight - playerHeight);
+    }
+
+    //check si il a eu une collision avec un mur
+    public boolean isCollision(TiledMapTileLayer obstacleLayer, float x, float y) {
+        int cellX = (int) (x / obstacleLayer.getTileWidth());
+        int cellY = (int) (y / obstacleLayer.getTileHeight());
+
+        TiledMapTileLayer.Cell cell = obstacleLayer.getCell(cellX, cellY);
+        return cell != null && cell.getTile() != null;
+    }
+
+    //revenir a lancienne case en case de collisions
+    public void handleCollisionWithObstacle(TiledMapTileLayer obstacleLayer) {
+        x = prevX;
+        y = prevY;
+    }
+
+    // Method to update previous position
+    public void updatePreviousPosition() {
+        prevX = x;
+        prevY = y;
+    }
 
 }
+
